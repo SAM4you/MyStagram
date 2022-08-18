5935d24e4c045ea39dfe9196d6e36e75054884ae
SAM4you <ander.stlouis@gmail.com>
2022-08-18 18:17:22 -0400

    Commit 2

--- app/src/main/java/com/ander/mystagram/MainActivity.java
+++ app/src/main/java/com/ander/mystagram/MainActivity.java
@@ -3,12 +3,57 @@ package com.ander.mystagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    private EditText etDescription;
+    private Button btnCam;
+    private ImageView ivCam;
+    private Button btnPost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        etDescription = findViewById(R.id.etDescription);
+        btnCam = findViewById(R.id.btnCam);
+        ivCam =  findViewById(R.id.ivCam);
+        btnPost = findViewById(R.id.btnPost);
+
+        queryPosts();
     }
+
+    private void queryPosts() {
+        // Specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        // Specify the object id
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e !=null){
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                } for (Post post : posts) {
+                    Log.i(TAG, "Post: "+ post.getDescription());
+                }
+
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/ander/mystagram/MyStagramApp.java
+++ app/src/main/java/com/ander/mystagram/MyStagramApp.java
@@ -3,12 +3,16 @@ package com.ander.mystagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class MyStagramApp extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("WIdGgiLi2Odb5DpfyfiBhWWStuozMdQsl619AV9j")
                 .clientKey("InCCGuM7Y6I0Opmv2u5SarGZcsx9riYfuKSHDWpI")
