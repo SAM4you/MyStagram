b6e0dc6a473ea9f9846b8f059b82d0c0cefa7fbb
SAM4you <ander.stlouis@gmail.com>
2022-08-23 00:47:21 -0400

    Project updated

--- app/src/main/java/com/ander/mystagram/MainActivity.java
+++ app/src/main/java/com/ander/mystagram/MainActivity.java
@@ -1,49 +1,20 @@
 package com.ander.mystagram;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
 
+import com.ander.mystagram.fragments.MyAccountFragment;
 import com.ander.mystagram.fragments.ComposeFragment;
 import com.ander.mystagram.fragments.FeedFragment;
-import com.ander.mystagram.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseFile;
-import com.parse.ParseQuery;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
-
-import java.io.File;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -101,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
 
                    case R.id.action_profile:
                        Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
-                       fragment = new ProfileFragment();
+                       fragment = new MyAccountFragment();
                        break;
                }
                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
---
+++ app/src/main/java/com/ander/mystagram/fragments/MyAccountFragment.java
@@ -0,0 +1,167 @@
+package com.ander.mystagram.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ander.mystagram.LoginActivity;
+import com.ander.mystagram.Post;
+import com.ander.mystagram.PostAdapter;
+import com.ander.mystagram.R;
+import com.ander.mystagram.User;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MyAccountFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MyAccountFragment extends Fragment {
+
+    public static final String TAG ="AccountFragment";
+
+    private User user;
+    private ImageView ivProfileImage;
+    private TextView tvScreenName;
+    private TextView tvBio; //Coming Soon
+    private Button btnLogout;
+    private RecyclerView rvFeed;
+    protected PostAdapter adapter;
+    protected List<Post> postList;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public MyAccountFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AccountFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MyAccountFragment newInstance(String param1, String param2) {
+        MyAccountFragment fragment = new MyAccountFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ivProfileImage = view.findViewById(R.id.ivProfileImage);
+        tvScreenName = view.findViewById(R.id.tvScreenName);
+        tvBio = view.findViewById(R.id.tvBio);
+        btnLogout = view.findViewById(R.id.btnLogout);
+        rvFeed = view.findViewById(R.id.rvFeed);
+
+
+        tvScreenName.setText(ParseUser.getCurrentUser().getUsername());
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser();
+
+                goLoginActivity();
+            }
+        });
+
+        // Create data source
+        postList = new ArrayList<>();
+
+        // Create Adapter
+        adapter = new PostAdapter(getContext(), postList);
+
+        // Set the Adapter
+        rvFeed.setAdapter(adapter);
+
+        // Set Layout Manager
+        rvFeed.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        queryPosts();
+
+
+    }
+
+    private void goLoginActivity() {
+        Intent intent = new Intent(getContext(), LoginActivity.class);
+        startActivity(intent);
+    }
+
+    protected void queryPosts() {
+        // Specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
+        query.addDescendingOrder(Post.KEY_UPDATED_AT);
+        // Specify the object id
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e !=null){
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                } for (Post post : posts) {
+                    Log.i(TAG, "Post: "+ post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+                postList.addAll(posts);
+                adapter.notifyDataSetChanged();
+
+            }
+        });
+    }
+
+
+
+}
